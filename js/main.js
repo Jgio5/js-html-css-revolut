2832a965200abda231a695131854b8ad3fbaf60c
Jorge Fabrizio Lopez Quiroz <giorgiofabrilopez@gmail.com>
2021-03-09 00:33:40 +0100

    Esercizio Completato

--- js/main.js
+++ js/main.js
@@ -1,5 +1,45 @@
-$(document).ready(function() {
-    $('.azienda').click(function () {
-        $('.menu-azienda').css('display','block');
+//Metodo .toggleClass 
+// $(document).ready(function() {
+//     $('.azienda').click(function () {
+//         $('.menu-azienda').toggleClass('active');
+//     });
+// });
+
+
+//Metodo .hide .show
+// var isVisible = true;
+
+// $(document).ready(function() {
+//     $('.azienda').click(function () {
+//         if(isVisible) {
+//             $('.menu-azienda').hide(); 
+//             isVisible = false;
+//         }
+//         else {
+//             $('.menu-azienda').show(); 
+//             isVisible = true;
+//         }
+//     });
+// });
+
+//Metodo .fadeIn .fadeOut
+// $(document).ready(function() {
+//     $('.azienda').mouseenter( function() {
+//             $('.menu-azienda').fadeIn(500);
+//     }); 
+
+//     $('.azienda').mouseleave( function() {
+//             $('.menu-azienda').fadeOut(500);
+//     });
+// });
+
+//Metodo .SlideDown .SlideUp
+$(document).ready(function(){
+    $('.azienda').click(function() {
+        $('.menu-azienda').slideDown();
     });
-});
+
+    $('.menu-azienda').mouseleave(function() {
+        $('.menu-azienda').slideUp();
+    });
+  });
